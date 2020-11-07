7409c2e5ced1fd53ada86b18b15d37e931fee227
Nikita <nikaleks2014@gmail.com>
2020-11-08 00:07:12 +0200

    Add

--- src/main/java/nikita/ne/you/ServletInitializer.java
+++ src/main/java/nikita/ne/you/ServletInitializer.java
@@ -1,13 +1,13 @@
-//package nikita.ne.you;
-//
-//import org.springframework.boot.builder.SpringApplicationBuilder;
-//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-//
-//public class ServletInitializer extends SpringBootServletInitializer {
-//
-//    @Override
-//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-//        return application.sources(nikita.ne.you.Application.class);
-//    }
-//
-//}
+package nikita.ne.you;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(nikita.ne.you.Application.class);
+    }
+
+}
